c4fececaea96dd9321feea2dcfcdb461d8951f9f
Carrick Rogers <carrickr@umich.edu>
2018-05-22 13:03:44 -0400

    update the fixture data to include a file with versions
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- spec/fcrepo_cloud_migrator/files_spec.rb
+++ spec/fcrepo_cloud_migrator/files_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe FcrepoCloudMigrator::Files do
   describe '.find_all_files' do
     it 'collects all ttl files containing the ebucore#filename predicate' do
       expect(described_class.new('spec/fixtures/data/').find_all_files)
-        .to contain_exactly 'spec/fixtures/data/73/7m01bk73c/files/5e19909f-8f36-4a8f-b3e0-f4e81c13a587/fcr%3Ametadata.ttl'
+        .to contain_exactly 'spec/fixtures/data/01/3r074v01q/files/84df4a72-1149-4438-874d-495aef0d84ca/fcr%3Ametadata.ttl',
+                            'spec/fixtures/data/01/3r074v01q/files/84df4a72-1149-4438-874d-495aef0d84ca/fcr%3Aversions/version1/fcr%3Ametadata.ttl',
+                            'spec/fixtures/data/01/3r074v01q/files/a0b9b7b5-de28-47eb-a22d-0a71686dc9dd/fcr%3Ametadata.ttl'
     end
   end
 end
