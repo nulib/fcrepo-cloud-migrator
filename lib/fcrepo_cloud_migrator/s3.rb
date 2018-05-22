c5f8185d93ebc412ec57ff254e09e0e52d44b087
Carrick Rogers <carrickr@umich.edu>
2018-05-22 16:57:23 -0400

    added fcrepocloudmigrator::s3.destination
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- lib/fcrepo_cloud_migrator/s3.rb
+++ lib/fcrepo_cloud_migrator/s3.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
+
 require 'pathname'
 
 module FcrepoCloudMigrator
   class S3
-    attr_reader :file, :graph
+    attr_reader :bucket, :checksum, :file, :graph
 
     EBUCORE_FILENAME_PREDICATE      = 'http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#filename'
     PREMIS_MESSAGE_DIGEST_PREDICATE = 'http://www.loc.gov/premis/rdf/v1#hasMessageDigest'
 
-    def initialize(file)
-      @file = file
-      @graph = RDF::Graph.load(file, format: :ttl)
+    def initialize(bucket:, file:)
+      @bucket      = bucket
+      @file        = file
+      @graph       = RDF::Graph.load(file, format: :ttl)
+      @checksum    = checksum_from_graph
     end
 
     def file_extension
@@ -23,10 +26,14 @@ module FcrepoCloudMigrator
       Pathname("#{Pathname(file).parent.parent}/#{File.basename(Pathname(file).parent)}.binary").to_s
     end
 
-    def checksum
+    def checksum_from_graph
       graph.each_statement do |statement|
         return statement.object.to_s.split(':').last if statement.predicate.to_s == PREMIS_MESSAGE_DIGEST_PREDICATE
       end
+    end
+
+    def destination
+      bucket + checksum
     end
   end
 end
