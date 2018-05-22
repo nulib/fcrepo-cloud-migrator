613e855117407abc9b71be95dd6dd8bfd8502aa1
Carrick Rogers <carrickr@umich.edu>
2018-05-22 11:51:46 -0400

    add check for files with multiple versions and log them
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- lib/fcrepo_cloud_migrator/files.rb
+++ lib/fcrepo_cloud_migrator/files.rb
@@ -4,6 +4,9 @@ module FcrepoCloudMigrator
   class Files
     attr_reader :directory
 
+    EBUCORE_FILENAME_PREDICATE    = 'http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#filename'
+    FEDORA_HAS_VERSIONS_PREDICATE = 'http://fedora.info/definitions/v4/repository#hasVersions'
+
     def initialize(directory)
       @directory = directory
     end
@@ -11,7 +14,10 @@ module FcrepoCloudMigrator
     def find_all_files
       [].tap do |fcrepo_file|
         Dir.glob("#{directory}**/*.ttl").map do |file|
-          fcrepo_file << file if contains_ebucore_filename?(file)
+          if contains_ebucore_filename?(file)
+            fcrepo_file << file
+            FcrepoCloudMigrator.logger.info("#{file} has multiple versions") if multiple_versions?(file)
+          end
         end
       end
     end
@@ -20,8 +26,16 @@ module FcrepoCloudMigrator
 
       def contains_ebucore_filename?(file)
         RDF::Graph.load(file, format: :ttl).each_statement.any? do |statement|
-          statement.predicate.to_s == 'http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#filename'
+          statement.predicate.to_s == EBUCORE_FILENAME_PREDICATE
         end
+      end
+
+      def multiple_versions?(file)
+        [].tap do |versions|
+          RDF::Graph.load(file, format: :ttl).each_statement do |statement|
+            versions << statement if statement.predicate.to_s == FEDORA_HAS_VERSIONS_PREDICATE
+          end
+        end.uniq.any?
       end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  FcrepoCloudMigrator.logger.level = Logger::ERROR
 end
