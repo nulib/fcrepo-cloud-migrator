f89a482809bc10d0a031de00573722abb6770ff2
Carrick Rogers <carrickr@umich.edu>
2018-05-23 13:26:06 -0400

    implement .upload_binary in FcrepoCloudMigrator::Upload
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>
    Co authored-by: Michael Klein <mbklein@gmail.com>

--- lib/fcrepo_cloud_migrator/upload.rb
+++ lib/fcrepo_cloud_migrator/upload.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
+require 'aws-sdk-s3'
+
 module FcrepoCloudMigrator
   class Upload
+    attr_accessor :binary, :bucket, :region
+
+    def initialize(bucket:, binary:, region: 'us-east-1')
+      @bucket = bucket
+      @binary = binary
+      @region = region
+    end
+
+    def upload_binary
+      begin
+        resource = Aws::S3::Resource.new(region: region)
+        obj = resource.bucket(bucket).object(binary.checksum_from_graph)
+        obj.upload_file(binary.path)
+      rescue Aws::S3::MultipartUploadError => e
+        FcrepoCloudMigrator.logger.error("Failed to multipart upload binary #{binary} with MultipartUploadError #{e.errors}")
+        false
+      rescue => e
+        FcrepoCloudMigrator.logger.error("Failed to upload binary #{binary} with error #{e}")
+        false
+      end
+    end
   end
 end
--- spec/fcrepo_cloud_migrator/upload_spec.rb
+++ spec/fcrepo_cloud_migrator/upload_spec.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
 RSpec.describe FcrepoCloudMigrator::Upload do
+  let(:binary) { FcrepoCloudMigrator::Binary.new(file: 'spec/fixtures/data/01/3r074v01q/files/84df4a72-1149-4438-874d-495aef0d84ca/fcr%3Ametadata.ttl')}
+  let(:upload) { described_class.new(bucket: 'mybucket-123', binary: binary) }
+  let(:logger) { instance_spy('logger') }
+
+  describe '.upload_binary' do
+    context 'success' do
+      before do
+        allow(Aws::S3::Resource).to receive(:new).and_return(Aws::S3::Resource.new(stub_responses: true))
+      end
+
+      it 'uploads the file' do
+        expect(upload.upload_binary).to be_truthy
+      end
+    end
+
+    context 'error' do
+      before do
+        expect(FcrepoCloudMigrator).to receive(:logger).and_return(logger)
+      end
+
+      it 'handles and logs errors' do
+        allow(Aws::S3::Resource).to receive(:new).and_raise(RuntimeError)
+        expect(upload.upload_binary).to be_falsey
+        expect(logger).to have_received(:error).once
+      end
+
+      it 'handles and logs Aws::S3::MultipartUploadError' do
+        allow(Aws::S3::Resource).to receive(:new).and_raise(Aws::S3::MultipartUploadError)
+        expect(upload.upload_binary).to be_falsey
+        expect(logger).to have_received(:error).once
+      end
+    end
+  end
 end
